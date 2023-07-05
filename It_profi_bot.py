bb7c9a6f8a9c625033fb068f2e801406c516a62b
Dmitriy <999gothicman999@mail.ru>
2023-07-05 22:23:08 +0700

    my first telegram bot

--- It_profi_bot.py
+++ It_profi_bot.py
@@ -4,8 +4,6 @@ import time
 
 TOKEN = '5871677585:AAFLrb03ZuFRlsq3cnOQ_Dplq3DRRJ7j_EA'
 
-BOT_URL = f'https://api.telegram.org/bot{TOKEN}'
-
 proxies = {
     'http': '221.141.158.183:80',
     'https': '117.251.103.186:8080',
@@ -15,15 +13,28 @@ apihelper.proxy = proxies
 bot = telebot.TeleBot(TOKEN)
 
 
+@bot.message_handler(commands=['start'])
+def command_start_function(message):
+    bot.reply_to(message, 'Рад Вас приветствовать!')
+
+
+@bot.message_handler(commands=['admin'], func=lambda message: message.from_user.id == 5361274567)
+def admin(message):
+    print(message)
+    bot.reply_to(message, 'Добрый день, хозяин!')
+
+
+@bot.message_handler(commands=['admin_'])
+def admin_(message):
+    if (message.from_user.id == 5361274567):
+        bot.reply_to(message, 'Добрый день, хозяин!')
+    else:
+        bot.reply_to(message, 'Ты не мой хозяин!')
+
 @bot.message_handler(content_types=['text'])
 def recieve_text(message):
     text = message.text
     bot.reply_to(message, f'Добрый день! Вы сказали : {text.upper()}')
-
-
-@bot.message_handler(commands=['start'])
-def command_start_function(message):
-    bot.reply_to(message, 'Рад Вас приветствовать!')
 
 
 bot.polling()
