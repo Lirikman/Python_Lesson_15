5f8b7bb02e2861f16c3cedbe100e9d5b7ff3303c
Dmitriy <999gothicman999@mail.ru>
2023-07-17 22:46:29 +0700

    my first telegram bot

--- It_profi_bot.py
+++ It_profi_bot.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import apihelper
+from telebot import types
 import time
 
 TOKEN = '5871677585:AAFLrb03ZuFRlsq3cnOQ_Dplq3DRRJ7j_EA'
@@ -12,29 +13,22 @@ proxies = {
 apihelper.proxy = proxies
 bot = telebot.TeleBot(TOKEN)
 
-
-@bot.message_handler(commands=['start'])
-def command_start_function(message):
-    bot.reply_to(message, 'Рад Вас приветствовать!')
-
-
-@bot.message_handler(commands=['admin'], func=lambda message: message.from_user.id == 5361274567)
-def admin(message):
-    print(message)
-    bot.reply_to(message, 'Добрый день, хозяин!')
-
-
-@bot.message_handler(commands=['admin_'])
-def admin_(message):
-    if (message.from_user.id == 5361274567):
-        bot.reply_to(message, 'Добрый день, хозяин!')
-    else:
-        bot.reply_to(message, 'Ты не мой хозяин!')
-
 @bot.message_handler(content_types=['text'])
-def recieve_text(message):
-    text = message.text
-    bot.reply_to(message, f'Добрый день! Вы сказали : {text.upper()}')
+def get_text_messages(message):
+    if message.text.lower() == 'привет' or message.text.lower() == 'добрый день':
+        bot.send_message(message.from_user.id, 'Привет! Давай посмотрим на список твоих дел!')
+        keyboard = types.InlineKeyboardMarkup()
+        key_tasks = types.InlineKeyboardButton(text = 'Список всех дел', callback_data='tasks')
+        keyboard.add(key_tasks)
+        key_add_task = types.InlineKeyboardButton(text='Добавить новую задачу', callback_data='add_task')
+        keyboard.add(key_add_task)
+        key_remove_task = types.InlineKeyboardButton(text='Удалить задачу', callback_data='remove_task')
+        keyboard.add(key_remove_task)
+        bot.send_message(message.from_user.id, text='Выбери нужную кнопку', reply_markup=keyboard)
+    elif message.text == '/help':
+        bot.send_message(message.from_user.id, 'Напиши Привет или Добрый день')
+    else:
+        bot.send_message(message.from_user.id, 'Я тебя не понимаю. Напиши Привет или Добрый день')
 
 
 bot.polling()
